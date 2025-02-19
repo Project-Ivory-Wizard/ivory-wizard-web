6894bbd4fd9cb72d36aaa89c02571ad5920b81ba
Sadie Conway <sadieconway2@gmail.com>
2025-02-18 22:43:46 -0500

    added data.tsx

--- template/frontend/src/Products/Products.tsx
+++ template/frontend/src/Products/Products.tsx
@@ -1,7 +1,10 @@
+import data from "./data";
 function Products(){
 return(
-    <div>
-        <h2>Products</h2>
+    <div className ="content">
+        <ul className="products">
+        </ul>
+
     </div>
 );
 }
---
+++ template/frontend/src/Products/data.tsx
@@ -0,0 +1,27 @@
+export default{
+    products:[{
+        name: 'Shirt', 
+        imageUrl: '/images/d1.jpg',
+        price: 60, 
+        brand: 'Nike',
+        rating: 4.5, 
+        numberOfReviews: 10, 
+
+    },
+{
+    name: 'Shirt',
+    imageUrl: '/images/d1.jpg',
+    price: 60, 
+    brand: 'Nike',
+    rating: 4.5, 
+    numberOfReviews: 10,
+},
+{
+    name: 'Shirt',
+    imageUrl: '/images/d1.jpg',
+    price: 80, 
+    brand: 'Nike',
+    rating: 4.0, 
+    numberOfReviews: 15,  
+}]
+}
